0a3c9fbc439c1c8c02f9b447df6671550c893490
Cong Dinh <congdinh2012@hotmail.com>
2024-12-20 14:38:25 +0700

    Step 5: Demo with Routing Navigate

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+    const navigate = useNavigate();
+
+    const gotoAbout = () => {
+        navigate('/about');
+    }
+
     return (
         <section>
             <h1 className="text-2xl font-semibold">Home Page</h1>
+            <button className="p-2 px-4 bg-blue-500 rounded-full text-white" onClick={gotoAbout}>
+                Go to About
+            </button>
         </section>
     );
 }
